4d9fe4caa9d480dfdd308d7a852474d04efcfa2c
Swagat Mitra <122202461+swagatmitra22@users.noreply.github.com>
2024-07-06 01:20:43 +0530

    Update MusicRecommendations.jsx

--- frontend/src/components/MusicRecommendations.jsx
+++ frontend/src/components/MusicRecommendations.jsx
@@ -1,5 +1,8 @@
+// MusicRecommendations.jsx
 import React, { useEffect, useState } from 'react';
+import Sidebar from './Sidebar'; // Import Sidebar component
 import '../styles/MusicRecommendations.css';
+import '../styles/Sidebar.css'; // Import Sidebar styles
 
 const MusicRecommendations = () => {
   const [tracks, setTracks] = useState([]);
@@ -22,27 +25,30 @@ const MusicRecommendations = () => {
   const handleTrackChange = (e) => setSeedTracks(e.target.value);
 
   return (
-    <div className="music-recommendations">
-      <div className="inputs">
-        <input type="text" placeholder="Search by Genre" value={seedGenres} onChange={handleGenreChange} />
-        {/* <input type="text" placeholder="Seed Artists" value={seedArtists} onChange={handleArtistChange} />
-        <input type="text" placeholder="Seed Tracks" value={seedTracks} onChange={handleTrackChange} /> */}
-      </div>
-      <div className="track-list">
-        {tracks.map((track) => (
-          <div key={track.id} className="track-card">
-            <div className="track-image">
-              <img src={track.album.images[0].url} alt={track.name} />
+    <div className="app-container">
+      <Sidebar /> {/* Include Sidebar component */}
+      <div className="music-recommendations">
+        <div className="inputs">
+          <input type="text" placeholder="Search by Genre" value={seedGenres} onChange={handleGenreChange} />
+          {/* <input type="text" placeholder="Seed Artists" value={seedArtists} onChange={handleArtistChange} />
+          <input type="text" placeholder="Seed Tracks" value={seedTracks} onChange={handleTrackChange} /> */}
+        </div>
+        <div className="track-list">
+          {tracks.map((track) => (
+            <div key={track.id} className="track-card">
+              <div className="track-image">
+                <img src={track.album.images[0].url} alt={track.name} />
+              </div>
+              <div className="track-info">
+                <h3>{track.name}</h3>
+                <p>{track.artists.map(artist => artist.name).join(', ')}</p>
+                <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="spotify-link">
+                  Listen on &nbsp;<i className="fa-brands fa-spotify"></i>
+                </a>
+              </div>
             </div>
-            <div className="track-info">
-              <h3>{track.name}</h3>
-              <p>{track.artists.map(artist => artist.name).join(', ')}</p>
-              <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="spotify-link">
-                Listen on &nbsp;<i class="fa-brands fa-spotify"></i>
-              </a>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
