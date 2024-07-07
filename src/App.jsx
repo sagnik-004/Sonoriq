8443ac1a8c62836717a3756c4307ae4ccdd1bbc2
Swagat Mitra <122202461+swagatmitra22@users.noreply.github.com>
2024-07-08 03:22:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
-import RecentUpdates from './components/RecentUpdates/RecentUpdates';
-import TopTracks from './components/TopTracks/TopTracks';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import RecentUpdates from "./components/RecentUpdates/RecentUpdates";
+import TopTracks from "./components/TopTracks/TopTracks";
 
 const Container = styled.div`
+  font-family: "Space Grotesk";
   display: flex;
   flex-direction: row;
   padding: 20px;
@@ -17,7 +19,7 @@ const LeftPane = styled.div`
 const RightPane = styled.div`
   width: 30%;
   padding-left: 20px;
-  border-left: 1px solid #ddd;
+  border-left: 3px solid rgba(255, 255, 255, 0.1);
 `;
 
 const App = () => {
