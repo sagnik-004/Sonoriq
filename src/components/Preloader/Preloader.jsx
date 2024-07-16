e6783863f8869d049b478a45c2da35a46cc94841
Samanyu Deghuria <samanyu.deghuria2022@iem.edu.in>
2024-07-16 23:50:26 +0530

    Add files via upload

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,11 @@
+// Preloader.jsx
+import React from 'react';
+import './Preloader.css'; // Make sure to create this CSS file
+
+const Preloader = () => (
+  <div className="preloader">
+    <div className="spinner"></div>
+  </div>
+);
+
+export default Preloader;
