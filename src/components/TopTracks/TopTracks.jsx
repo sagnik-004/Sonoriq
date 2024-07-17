f9d6e0285ab3a1a50289c1fb05a7598711493afd
Samanyu Deghuria <samanyu.deghuria2022@iem.edu.in>
2024-07-18 02:35:10 +0530

    Update TopTracks.jsx- switch done

--- src/components/TopTracks/TopTracks.jsx
+++ src/components/TopTracks/TopTracks.jsx
@@ -5,13 +5,14 @@ import './TopTracks.css';
 const MusicCharts = () => {
   const [topTracks, setTopTracks] = useState([]);
   const [topArtists, setTopArtists] = useState([]);
+  const [showTopTracks, setShowTopTracks] = useState(true); // State to toggle between top tracks and top artists
 
   useEffect(() => {
     const fetchSpotifyData = async () => {
       const clientId = 'd91a526fac8d4ce59f314782764bca69';
       const clientSecret = '8c16b55a114047e98523196eb625fc71';
       const playlistId = '37i9dQZF1DXcBWIGoYBM5M'; // Replace with your playlist ID
-      const artistIds = '1Xyo4u8uXC1ZmMpatF05PJ,5K4W6rqBFWDnAN6FQUkS6x,7dGJo4pcD2V6oG8kP0tJRR'; // Replace with your artist IDs
+      const artistIds = '1Xyo4u8uXC1ZmMpatF05PJ,5K4W6rqBFWDnAN6FQUkS6x,7dGJo4pcD2V6oG8kP0tJRR,66CXWjxzNUsdJxJ2JdwvnR,6eUKZXaKkcviH0Ku9w2n3V'; // Replace with your artist IDs
 
       try {
         // Get Spotify access token
@@ -49,27 +50,48 @@ const MusicCharts = () => {
 
   return (
     <div className="music-charts">
-      <h2>Top Tracks</h2>
-      <ul className="top-tracks">
-        {topTracks.map((track, index) => (
-          <li key={index} className="track-item">
-            <img src={track.track.album.images[0].url} alt={track.track.name} />
-            <div className="track-info">
-              <span className="track-title">{track.track.name}</span>
-              <span className="track-artist">{track.track.artists[0].name}</span>
-              <a href={track.track.external_urls.spotify} className="listen-link" target="_blank" rel="noopener noreferrer">
-              Listen on &nbsp;<i className="fa-brands fa-spotify"></i>
-              </a>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h2>Top Artists</h2>
-      <ul className="top-artists">
-        {topArtists.map((artist, index) => (
-          <li key={index}>{artist.name}</li>
-        ))}
-      </ul>
+      <div className="switch-button">
+        <button className={showTopTracks ? 'active' : ''} onClick={() => setShowTopTracks(true)}>Top Tracks</button>
+        <button className={!showTopTracks ? 'active' : ''} onClick={() => setShowTopTracks(false)}>Top Artists</button>
+      </div>
+      {showTopTracks ? (
+        <div>
+          <h2>Top Tracks</h2>
+          <ul className="top-tracks">
+            {topTracks.map((track, index) => (
+              <li key={index} className="track-item">
+                <img src={track.track.album.images[0].url} alt={track.track.name} />
+                <div className="track-info">
+                  <span className="track-title">{track.track.name}</span>
+                  <span className="track-artist">{track.track.artists[0].name}</span>
+                  <a href={track.track.external_urls.spotify} className="listen-link" target="_blank" rel="noopener noreferrer">
+                    Listen on &nbsp;<i className="fa-brands fa-spotify"></i>
+                  </a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <h2>Top Artists</h2>
+          <ul className="top-artists">
+            {topArtists.map((artist, index) => (
+              <li key={index} className="artist-item">
+                <img src={artist.images[0].url} alt={artist.name} className="artist-img" />
+                <div className="artist-info">
+                  <a href={artist.external_urls.spotify} className="artist-name" target="_blank" rel="noopener noreferrer">
+                    {artist.name}
+                  </a>
+                  <span className="artist-genres">Genres: {artist.genres.join(', ')}</span>
+                  <span className="artist-followers">Followers: {artist.followers.total.toLocaleString()}</span>
+                  <span className="artist-popularity">Popularity: {artist.popularity}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
