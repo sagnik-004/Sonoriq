d781d5a390ca9c8fc493859fba160e529cbb66e4
Sourish Sarkar <sourishsarkar0011@gmail.com>
2024-07-20 00:28:05 +0530

    Update firebase.js

--- src/components/LoginRegister/firebase.js
+++ src/components/LoginRegister/firebase.js
@@ -1,10 +1,25 @@
-// Import the necessary functions from Firebase SDKs
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
-import { getFirestore, doc, setDoc, getDoc, query, where, collection, getDocs } from "firebase/firestore";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signInWithPopup, 
+  GoogleAuthProvider, 
+  sendPasswordResetEmail 
+} from "firebase/auth";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  query, 
+  where, 
+  collection, 
+  getDocs 
+} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCsbyDmM5DcQBMhV2Lh7_CpUbIFcuDC2IE",
   authDomain: "sonoriq.firebaseapp.com",
@@ -18,16 +33,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
+// Initialize Firebase services
 const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
-// Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
 
-// Export the initialized services and necessary methods for use in other parts of your app
+// Export Firebase services and methods
 export {
   auth,
   createUserWithEmailAndPassword,
